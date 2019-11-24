b0c2b6d43bea09f57f7e64d1fd8a7a3448db0a7d
alfredpang01 <32011931+alfredpang01@users.noreply.github.com>
2019-11-24 19:13:39 +0800

    Add files via upload

---
+++ TCPclient.py
@@ -0,0 +1,45 @@
+"""
+
+Created and Done By:
+
+1. Alfred Pang Wei Yang 1700521
+
+2. Lim Jie Chi 1700421
+
+3. See Toh Sheng Yang 1700154
+
+"""
+
+from socket import *
+import sys
+from time import *
+
+
+
+try:
+    serverName = raw_input('Please enter server IP address: ')
+    serverPort = 7
+    # initiate 3 way handshake TCP connection with server
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+
+except gaierror:
+    print 'Error, could not connect with server'
+    sys.exit(1)
+
+while 1:
+    sentence = raw_input('Input sentence: ')
+    if sentence != 'terminate':
+        # send TCP byte stream to server
+        clientSocket.send(sentence)
+        # receive TCP byte stream from server
+        bufferSize = 2048
+        receivedSentence = clientSocket.recv(bufferSize)
+        print 'From Server: ', receivedSentence
+    else:
+        print 'Connection closing......'
+        break
+
+#close TCP connection
+clientSocket.close()
+
---
+++ TCPserver.py
@@ -0,0 +1,55 @@
+"""
+
+Created and Done By:
+
+1. Alfred Pang Wei Yang 1700521
+
+2. Lim Jie Chi 1700421
+
+3. See Toh Sheng Yang 1700154
+
+"""
+
+import socket
+import threading
+
+class ThreadedServer(object):
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def listen(self):
+        self.sock.listen(1)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target = self.listenToClient,args = (client,address)).start()
+
+    def listenToClient(self, client, address):
+        size = 1024
+        while True:
+            try:
+                data = client.recv(size)
+                response = data
+                client.send(response)
+
+            except:
+                client.close()
+                return False
+
+if __name__ == "__main__":
+    while 1:
+        try:
+            port_num = 7
+            print 'The server is ready to receive'
+            break
+        except ValueError:
+            pass
+
+    ThreadedServer('',port_num).listen()
+# open TCP socket to listen at well-known port
+
+
